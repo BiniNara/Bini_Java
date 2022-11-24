1fca362a9966604d9715165b22ff45e9bc963fbb
BiniNara <gusqls5507@naver.com>
2022-11-24 12:23:28 +0900

    221124 저장

---
+++ Bini_Java/src/ch03_test_1018/Hello.java
@@ -0,0 +1,19 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 실행은 ctrl + f11
+		// 주석은 보통 ctrl + / : 단일 행 주석.
+		// 한번 더 ctrl + / : 주석 취소
+		// 여러 행 주석 : ctrl + shift + /
+		// 여러 행 주석 해제 : ctrl + shift + \
+		// 자동 포맷 정렬 : ctrl + shift + f
+		
+        System.out.println("Hello 이상용 자바");
+        System.out.println("ㅎㅇㅎㅎㅇ");
+        
+	}
+
+}
